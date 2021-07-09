2b8618fa72fb87e3fa8876fc1cc97d1310d35e9d
leandromuzzupappa <leann.muzzu@gmail.com>
2021-07-09 05:45:51 -0300

    Se cargan los datos desde el json

--- controllers/catalogControllers.js
+++ controllers/catalogControllers.js
@@ -1,17 +1,18 @@
 const path = require("path");
 const fs = require("fs");
 
-let data = fs.readFileSync("/database/data.json", { encoding: "utf-8" });
-
 const catalogController = {
     getOne: (req, res) => {
         return res.send("<h1>Producto</h1>");
     },
     getAll: (req, res) => {
         // Llamado a la db
-        console.log(data);
+        let db = fs.readFileSync("database/data.json", { encoding: "utf-8" });
+        const allProducts = JSON.parse(JSON.parse(db));
 
-        return res.render("index");
+        return res.render("index", {
+            products: allProducts,
+        });
     },
 };
 
